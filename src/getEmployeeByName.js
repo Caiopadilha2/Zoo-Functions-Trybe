7784bce01b25e2b2815893af0671fb2e5bec91c5
Caio <caiopadilha@id.uff.br>
2022-01-09 15:52:21 -0300

    Requisito 3 - Faltando entender algumas coisas e se dá para tirar o IF

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const funcionario = employees.find((employee) => employeeName === employee.firstName
+   || employeeName === employee.lastName);
+  return funcionario;
+  // Fiz 100% sozinho, sem pesquisar. Estava fazendo (employeeName === employee.firstName
+  // || employee.lastName) e não estava dando certo os testes 2 e 3. Depois de arrumar isso, fiz a linha 6 e 7
+  // para passar o requisito 1, mas acho que deve ter algum jeito de não precisar daquele IF.
 }
 
 module.exports = getEmployeeByName;
