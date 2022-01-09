03f2772ac4c5830b6eb334993cca0d84f923ea46
Caio <caiopadilha@id.uff.br>
2022-01-09 15:34:10 -0300

    Requisito 2 - Faltando entender dif find X filter

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalParaAchar = species.find((specie) => animal === specie.name);
+  // // Pq não é para usar filter e sim find? Filter vai retornar um array de objetos com o elemento inteiro.
+  const idadeMinima = animalParaAchar.residents.every((resident) => resident.age >= age);
+
+  return idadeMinima;
+  // return species.every((specie) => age > specie.residents.age);
 }
 
 module.exports = getAnimalsOlderThan;
