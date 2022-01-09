692901b50183c5b0c06c1a7ad32dbbf2fde89ffc
Caio <caiopadilha@id.uff.br>
2022-01-09 16:21:23 -0300

    Requisito 2 - Ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,11 +4,14 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const animalParaAchar = species.find((specie) => animal === specie.name);
+  // console.log(animalParaAchar);
   // // Pq não é para usar filter e sim find? Filter vai retornar um array de objetos com o elemento inteiro.
-  const idadeMinima = animalParaAchar.residents.every((resident) => resident.age >= age);
-
-  return idadeMinima;
-  // return species.every((specie) => age > specie.residents.age);
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+  // Entendi porque usar find. Ele retorna todo o elemento, no caso era um objeto.
+  const temIdadeMinima = animalParaAchar.residents.every((resident) => resident.age >= age);
+  // Se eu colocar o mouse em cima de ''animalParaAchar'', vejo tudo o que ele é/retornou lá de cima.
+  return temIdadeMinima;
 }
+// getAnimalsOlderThan('tigers', 2);
 
 module.exports = getAnimalsOlderThan;
