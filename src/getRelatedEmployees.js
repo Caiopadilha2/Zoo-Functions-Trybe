a5b96d69f73c7362ce974b22ea43958db26c6393
Caio <caiopadilha@id.uff.br>
2022-01-10 16:18:17 -0300

    Testes 1 e 2 de 4 do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  // 'retorna true se o id passado é de um gerente e false se não for de um gerente.'
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  // retorna uma array com o nome dos funcionários do respectivo gerente passado pelo id' OU 'dispara erro com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
 }
 
 module.exports = { isManager, getRelatedEmployees };
