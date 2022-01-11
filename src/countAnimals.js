35041692d1e0a43780164d7207eb4171bb9051e8
Caio <caiopadilha@id.uff.br>
2022-01-10 22:07:22 -0300

    Teste 1/3 do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const numeroDeAnimais = species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+    return numeroDeAnimais;
+  }
 }
 
 module.exports = countAnimals;
